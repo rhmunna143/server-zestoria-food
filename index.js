bef86f7968027cfcd81c5077a75f569036592dde
Munna <munna@munna.127.0.0.1>
2023-10-17 22:27:59 +0600

    mongodb connected with server

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 7000;
 
+// middlewares
+app.use(cors())
+app.use(express.json())
+
+// test response
 app.get("/", (req, res) => {
-    res.send("running zestoria server : 200 OK!")
+  res.send("running zestoria server : 200 OK!")
 })
 
 
 
 
+// mongodb driver config
+
+// mongo URI
+const uri = `mongodb+srv://${process.env.USER_ZESTORIA}:${process.env.PASS_ZESTORIA}@cluster0.52hv04l.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 
+
+// express listener
 app.listen(port, () => {
-    console.log(`zestoria listening on port ${port}`);
+  console.log(`zestoria listening on port ${port}`);
 })
